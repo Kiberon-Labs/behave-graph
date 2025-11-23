ecadbca73ac547366fcd98e8e4785b1b3d644ec2
KiberonDev <210491275+kiberonlabsdev@users.noreply.github.com>
2025-11-23 16:38:49 +0200

    Clean graph editor

--- packages/graph-editor/tailwind.config.js
+++ packages/graph-editor/tailwind.config.js
@@ -2,7 +2,7 @@ export default {
   content: [
     './public/index.html',
     './src/**/*.{js,ts,jsx,tsx,css}',
-    '../../packages/flow/dist/**/*.{js,ts,jsx,tsx,css}'
+    '..//flow/dist/**/*.{js,ts,jsx,tsx,css}'
   ],
   theme: {
     extend: {}
